76a5de8e1c17871c30484c3e5e82244cc6577e0f
Davi <davirodrigues8@gmail.com>
2019-06-04 23:34:58 -0300

    adds corrections in function webscraping

--- src/br/ufrn/imd/lp2/controller/WebScrapingController.java
+++ src/br/ufrn/imd/lp2/controller/WebScrapingController.java
@@ -1,10 +1,8 @@
 package br.ufrn.imd.lp2.controller;
-
-import br.ufrn.imd.lp2.model.Quote;
-
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import org.jsoup.Jsoup;
@@ -15,48 +13,26 @@ import org.jsoup.select.Elements;
 
 public class WebScrapingController {
 
-	public Quote toCollectData(String url) {
-		Quote quote = new Quote();
+	public ArrayList<String> toCollectData(String url) {
 		
-		String created_at = "";
-		String content = "";
+		ArrayList<String> data = new ArrayList<String>();
+		
 		try {
 			Document doc = Jsoup.connect(url).get();
 			
-			// Get the created date or collect date
-			Elements datas =  doc.getElementsByAttribute("datetime");
-			
-			if (datas.size() > 0) {
-				created_at = datas.first().attr("datetime").toString();
-			} else {
-				Date date = new Date();
-				created_at = date.toString();
-			}
-			quote.setDate(created_at);
-			
-			// Get the paragraphs p:not(:has(span,iframe,a,label,input))
-			Elements paragraphs1 = doc.select("h1,h2");
-			Elements paragraphs = doc.select("h1,h2,p:not(:has(span,iframe,a,label,input)), p:root");
-			System.out.println(paragraphs1);
+			Elements paragraphs = doc.select("p");
 			
 			for (Element paragraph : paragraphs) {
-//				System.out.println(paragraph.is("h2"));
-				if (paragraph.hasText() && paragraph.text().length() > 108) {
-//					System.out.println(paragraph);
-//					content = content.concat(paragraph.text()+" ");
+				if (paragraph.text().length() > 152) {
+					data.add(paragraph.text());
 				}
 			}
-			quote.setContent(content);
-			
-			quote.setUrl(url);
-			
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		return quote;
+		return data;
 	}
 
 	public double measureAccuracy(String quote) {
