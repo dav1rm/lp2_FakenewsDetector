8e9af434f74a04712401b6473406d06127d61a1f
Davi <davirodrigues8@gmail.com>
2019-06-10 01:30:04 -0300

    adds result page

---
+++ src/br/ufrn/imd/lp2/controller/ResultController.java
@@ -0,0 +1,31 @@
+package br.ufrn.imd.lp2.controller;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ResultController {
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private ImageView character;
+
+    @FXML
+    private Text baloonText;
+
+    @FXML
+    private Text fakenews;
+
+    @FXML
+    private Text content;
+    
+    @FXML
+    void handleSubmit(ActionEvent event) {
+
+    }
+}
